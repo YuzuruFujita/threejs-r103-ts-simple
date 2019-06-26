c0f3d91df93530032c2d25309bd6030d7c217da6
3770k <3770k@example.com>
2019-06-27 01:46:17 +0900

    DRACOLoaderがjsmで対応されたので自前の定義を置き換えた。

--- three.combined.d.ts
+++ three.combined.d.ts
@@ -3,10 +3,6 @@ export * from './node_modules/three/src/Three';
 export * from './node_modules/three/examples/jsm/controls/OrbitControls'
 export * from './node_modules/three/examples/jsm/loaders/GLTFLoader'
 export * from './node_modules/three/examples/jsm/exporters/GLTFExporter'
-
-export declare class DRACOLoader {
-    public static setDecoderPath(path: string): DRACOLoader
-    public static getDecoderModule(): Promise<any>
-}
+export * from './node_modules/three/examples/jsm/loaders/DRACOLoader'
 
 export as namespace THREE;
